0c46324d74181bb56ed5c18c251d0c65918c4d2c
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-06-03 20:19:09 +0700

    masuk ke hyde

--- engine/bind/login/loginPage.bind.php
+++ engine/bind/login/loginPage.bind.php
@@ -4,7 +4,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1", shrink-to-fit="no">
-  <title>Nadha Laundry</title>
+  <title>Nadha School - Aplikasi Manajemen Sekolah</title>
   <!-- base:css -->
   <link rel="stylesheet" href="<?=STYLEBASE; ?>/login/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="<?=STYLEBASE; ?>/login/vendors/base/vendor.bundle.base.css">

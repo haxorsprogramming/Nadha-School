ab2d90d2c1cafee53cfb7f70d88a34f8ef75ff26
haxorsprogramming <haxorsuinsu@gmail.com>
2020-10-09 00:34:31 +0700

    update

--- engine/bind/dasbor/index.bind.php
+++ engine/bind/dasbor/index.bind.php
@@ -6,8 +6,8 @@
     <title>Dore jQuery</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <link rel="stylesheet" href="font/iconsmind-s/css/iconsminds.css" />
-    <link rel="stylesheet" href="font/simple-line-icons/css/simple-line-icons.css" />
+    <link rel="stylesheet" href="annisa/font/iconsmind-s/css/iconsminds.css" />
+    <link rel="stylesheet" href="annisa/font/simple-line-icons/css/simple-line-icons.css" />
 
     <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
     <link rel="stylesheet" href="css/vendor/bootstrap.rtl.only.min.css" />
@@ -760,12 +760,12 @@
     </footer>
 
 
-    <script src="js/vendor/jquery-3.3.1.min.js"></script>
-    <script src="js/vendor/bootstrap.bundle.min.js"></script>
-    <script src="js/vendor/perfect-scrollbar.min.js"></script>
-    <script src="js/vendor/mousetrap.min.js"></script>
-    <script src="js/dore.script.js"></script>
-    <script src="js/scripts.js"></script>
+    <script src="annisa/js/vendor/jquery-3.3.1.min.js"></script>
+    <script src="annisa/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="annisa/js/vendor/perfect-scrollbar.min.js"></script>
+    <script src="annisa/js/vendor/mousetrap.min.js"></script>
+    <script src="annisa/js/dore.script.js"></script>
+    <script src="annisa/js/scripts.js"></script>
 </body>
 
 </html>

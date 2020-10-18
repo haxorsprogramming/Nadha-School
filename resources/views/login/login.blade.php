de4c3d716e88d6c1d35bd1a015b3bb15764bdb14
haxorsprogramming <haxorsuinsu@gmail.com>
2020-10-19 04:41:43 +0700

    update

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -3,86 +3,109 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login - Nadha Resto</title>
+    <title>NadhaSchool - Homepage</title>
 
-    <!-- General CSS Files -->
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/fontawesome/css/all.min.css">
+        <meta name="description" content="Nadha School - Aplikasi Manajemen Sekolah">
+        <meta name="author" content="haxorsprogrammingclub">
+        <meta name="robots" content="noindex, nofollow">
 
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/bootstrap-social/bootstrap-social.css">
-
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/css/style.css">
-    <link rel="stylesheet" href="https://demo.getstisla.com/assets/css/components.css">
-
+        <!-- Stylesheets -->
+        <!-- Fonts and Codebase framework -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
+        <link rel="stylesheet" id="css-main" href="{{ asset('ladun/login/assets/css/codebase.min.css') }}">
+        <!-- <link rel="stylesheet" id="css-main" href="/login/assets/css/codebase.min.css"> -->
+        
 </head>
 
-<body onload="initF()">
-    <div id="app">
-        <section class="section">
-            <div class="container mt-5">
-                <div class="row">
-                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                        <div class="login-brand">
-                            <img src="{{ asset('ladun/login/img/nadha_resto.png') }}" alt="logo" width="200">
-                        </div>
+<body>
 
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h4>Login</h4>
+<div id="page-container" class="main-content-boxed">
+
+            <!-- Main Container -->
+            <main id="main-container">
+                <!-- Page Content -->
+                <div class="bg-image" style='background-image: url("{{ asset('ladun/login/assets/media/photos/cover_1.jpg') }}");'>
+                    <div class="row mx-0 bg-black-op">
+                        <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
+                            <div class="p-30 invisible" data-toggle="appear">
+                                <p class="font-size-h3 font-w600 text-white">
+                                    Spirit of Collaborative, never stop coding.
+                                </p>
+                                <p class="font-italic text-white-op">
+                                    Copyright &copy; Haxorsprogrammingclub - Support by NadhaMedia
+                                </p>
                             </div>
-
-                            <div class="card-body">
-                                    <div class="form-group">
-                                        <label for="email">Username</label>
-                                        <input type="text" class="form-control" id="txtUsername">
+                        </div>
+                        <div class="hero-static col-md-6 col-xl-4 d-flex align-items-center bg-white invisible" data-toggle="appear" data-class="animated fadeInRight">
+                            <div class="content content-full">
+                                <!-- Header -->
+                                <div class="px-30 py-10">
+                                    <div style="text-align: center;">
+                                        <img src="{{ asset('ladun/default/img/nadha_school.jpg') }}" style="width: 200px;">
                                     </div>
-                                    <div class="form-group">
-                                        <div class="d-block">
-                                            <label for="password" class="control-label">Password</label>
+                                    <h1 class="h3 font-w700 mt-30 mb-10">Welcome to NadhaSchool</h1>
+                                    <h2 class="h5 font-w400 text-muted mb-0">Please sign in</h2>
+                                </div>
+                               
+                                <div class="js-validation-signin px-30" id="divForm">
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="text" class="form-control" id="txtUsername">
+                                                <label for="login-username">Username</label>
+                                            </div>
                                         </div>
-                                        <input type="password" class="form-control" id='txtPassword'>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="password" class="form-control" id="txtPassword">
+                                                <label for="login-password">Password</label>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="form-group">
-                                        <a class="btn btn-primary btn-lg btn-block btn-icon icon-left" href='#!' @click='loginAtc' id='btnLogin'>
-                                            <i class="fas fa-sign-in-alt"></i> 
-                                            Login
+                                        <a class="btn btn-sm btn-hero btn-alt-primary" @click="loginAtc">
+                                            <i class="si si-login mr-10"></i> Sign In
                                         </a>
-                                    </div>                             
-
+                                        <div class="mt-30">
+                                            <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="#!">
+                                                <i class="fa fa-plus mr-5"></i> Create Account
+                                            </a>
+                                            <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="#!">
+                                                <i class="fa fa-warning mr-5"></i> Forgot Password
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- END Sign In Form -->
                             </div>
-                        </div>
-                        <div class="simple-footer">
-                            Copyright &copy; @{{appName}}
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
+                <!-- END Page Content -->
+            </main>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/vue@next"></script>
+        
+        <script src="{{ asset('ladun/login/assets/js/codebase.core.min.js') }}"></script>
+        <script src="{{ asset('ladun/login/assets/js/codebase.app.min.js') }}"></script>
+        <!-- Page JS Plugins -->
+        <script src="{{ asset('ladun/login/assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+        <!-- Page JS Code -->
+        <script src="{{ asset('ladun/login/assets/js/pages/op_auth_signin.min.js') }}"></script>
 
-    <!-- General JS Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/jquery.min.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/popper.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/tooltip.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="https://demo.getstisla.com/assets/modules/moment.min.js"></script>
-    <script src="https://demo.getstisla.com/assets/js/stisla.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-    <!-- JS Libraies -->
+        <!-- Custom JS Code  -->
+        <script>
+            const server = "{{ url('') }}";
+        </script>
 
-    <!-- Page Specific JS File -->
-
-    <!-- Template JS File -->
-    <script src="https://demo.getstisla.com/assets/js/scripts.js"></script>
-    <script>
-        const server = "{{ url('') }}";
-    </script>
     <script src="{{ asset('ladun/login/js/login.js') }}"></script>
 </body>
 

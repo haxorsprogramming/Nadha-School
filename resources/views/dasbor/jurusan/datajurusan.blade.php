415837514a08037b3a96aad436f2b51edb8aa5af
haxorsprogramming <haxorsuinsu@gmail.com>
2020-10-27 03:27:16 +0700

    update

--- resources/views/dasbor/jurusan/datajurusan.blade.php
+++ resources/views/dasbor/jurusan/datajurusan.blade.php
@@ -48,37 +48,7 @@
     </div>
 </div>
 
-<div class="card card-primary" id="divFormTambahJurusan">
+@include('dasbor.jurusan.formtambahdatajurusan')
 
-    <div class="card-body">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                <div class="form-group">
-                    <label>Nama Jurusan</label>
-                    <input type="text" class="form-control" id="txtNamaJurusan">
-                </div>
-                <div class="form-group">
-                    <label>Prefix (Singkatan)</label>
-                    <input type="text" class="form-control" id="txtPrefix">
-                </div>
-                <div class="form-group">
-                    <label>Deks</label>
-                    <textarea class="form-control" style="resize: none;" id="txtDeks"></textarea>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                <div style="text-align: center;">
-                    <img src="{{ asset('ladun/dasbor/img/fig_2.png') }}" style="width: 250px;">
-                </div>
-            </div>
-            <div>
-                <a href="#!" class="btn btn-primary btn-icon icon-left" id="btnSimpan" @click="prosesTambahAtc">
-                    <i class="fas fa-save"></i> @{{ capBtnSimpan }} <img src="{{ asset('ladun/dasbor/loading.svg') }}" width="20" id="divLoading">
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
 
 <script src="{{ asset('ladun/dasbor/jurusan/datajurusan.js') }}"></script>
--- resources/views/dasbor/siswa/datasiswa.blade.php
+++ resources/views/dasbor/siswa/datasiswa.blade.php
@@ -1,54 +1,10 @@
 <div class="card card-primary" id="divDataSiswa">
-    <div class="card-body">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active show" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Basic</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Akun</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Keuangan</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="firebase-tab" data-toggle="tab" href="#firebase" role="tab" aria-controls="firebase" aria-selected="false">Firebase Setting</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="other-tab" data-toggle="tab" href="#other" role="tab" aria-controls="other" aria-selected="false">Lainnya</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade active show" id="home" role="tabpanel" aria-labelledby="home-tab">
-                <div class="form-group">
-                    <label>Nama Restoran</label>
-                    <input type="text" class="form-control" id='txtNamaResto' disabled>
-                </div>
-                <div class="form-group">
-                    <label>Alamat Restoran </label><small> Pisahkan daerah dengan koma (,). Cth Kec. Pariaman,
-                        Kab. Pariaman, Sumatera Barat</small>
-                    <input type="text" class="form-control" id='txtAlamatResto' disabled>
-                </div>
-                <div class="form-group">
-                    <label>Nama Owner</label>
-                    <input type="text" class="form-control" id='txtNamaOwnser' disabled>
-                </div>
-                <div class="form-group">
-                    <label>Nomor Handphone Restoran</label>
-                    <input type="text" class="form-control" id='txtHpRestoran' disabled>
-                </div>
-                <div class="form-group">
-                    <label>Email Restoran</label>
-                    <input type="text" class="form-control" id='txtEmailRestoran' disabled>
-                </div>
-            </div>
-            <div class="tab-pane fade active show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 
-            </div>
-        </div>
-    </div>
 </div>
 
 <div class="card card-primary" id="divTambahDataSiswa">
+
+@include('dasbor.siswa.formtambahdatasiswa')
 
 </div>
 

3e5a155c97ce42dff90075cd94e53a185be9ac6b
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-21 15:23:41 +0700

    update

--- resources/views/dasbor/siswa/formtambahsiswa/fotoprofil.blade.php
+++ resources/views/dasbor/siswa/formtambahsiswa/fotoprofil.blade.php
@@ -6,7 +6,7 @@
             <div style="text-align:center;">
                 <img src="{{ asset('ladun/dasbor/img/avatar-1.png') }}" class="rounded-circle profile-widget-picture" style="width:200px;">
                 <div style="margin-top:20px;">
-                    <a href="#!" class="btn btn-primary btn-large">Upload Foto</a>
+                    <a href="#!" class="btn btn-primary btn-large">Upload Foto Siswa</a>
                 </div>
             </div>
         </div>
@@ -14,6 +14,7 @@
         <div class="col-12 col-sm-6 col-lg-6">
 
         </div>
+
     </div>
 
 </div>

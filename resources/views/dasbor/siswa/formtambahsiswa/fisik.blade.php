06776b1bf16cc65a78f2f35c56498e8b9dfa9a06
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-25 00:09:20 +0700

    update

--- resources/views/dasbor/siswa/formtambahsiswa/fisik.blade.php
+++ resources/views/dasbor/siswa/formtambahsiswa/fisik.blade.php
@@ -9,25 +9,25 @@
         <div class="col-12 col-sm-6 col-lg-6">
             <div class="form-group">
                 <label>Tinggi Badan</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="txtTinggiBadan" name="txtTinggiBadan">
             </div>
             <div class="form-group">
                 <label>Berat Badan</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="txtBeratBadan" name="txtBeratBadan">
             </div>
         </div>
 
         <div class="col-12 col-sm-6 col-lg-6">
         <div class="form-group">
                 <label>Berkacamata</label>
-                <select class="form-control">
+                <select class="form-control" id="txtBerkacamata" name="txtBerkacamata">
                     <option value="0">Tidak</option>
                     <option value="1">Ya</option>
                 </select>
             </div>
             <div class="form-group">
                 <label>Buta Warna</label>
-                <select class="form-control">
+                <select class="form-control" id="txtButaWarna" name="txtButaWarna">
                     <option value="0">Tidak</option>
                     <option value="1">Ya</option>
                 </select>
--- resources/views/dasbor/siswa/formtambahsiswa/informasipendaftaran.blade.php
+++ resources/views/dasbor/siswa/formtambahsiswa/informasipendaftaran.blade.php
@@ -5,26 +5,26 @@
         <div class="col-12 col-sm-6 col-lg-6">
             <div class="form-group">
                 <label>Asal Sekolah</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="txtAsalSekolah" name="txtAsalSekolah">
             </div>
             <div class="form-group">
                 <label>No Ijazah</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="txtNoIjazah" name="txtNoIjazah">
             </div>
             <div class="form-group">
                 <label>Status Masuk</label>
-                <select class="form-control">
+                <select class="form-control" id="txtStatusMasuk" name="txtNoIjazah">
                     <option value='siswabaru'>Siswa baru</option>
                     <option value='pindahan'>Pindahan</option>
                 </select>
             </div>
             <div class="form-group">
                 <label>No Tes Masuk</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="txtTesMasuk" name="txtTesMasuk">
             </div>
             <div class="form-group">
                 <label>Tanggal Masuk</label>
-                <input type="date" class="form-control">
+                <input type="date" class="form-control" id="txtTanggalMasuk" name="txtTesMasuk">
             </div>
 
         </div>

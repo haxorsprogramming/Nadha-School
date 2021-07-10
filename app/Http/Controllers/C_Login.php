b5fa144cb96b5f83cc92d2e504636ac255fe4a62
Haxorsprogramming <haxors@uinsu.ac.id>
2021-07-11 04:56:11 +0700

    'update'

--- app/Http/Controllers/C_Login.php
+++ app/Http/Controllers/C_Login.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Arr;
 class C_Login extends Controller
 {
     public function login_page()
     {
-        return view('login.login_page');
+        $arrPic = ['1','2'];
+        $pic = Arr::random($arrPic);
+        $pathPic = asset('ladun/login/assets/media/photos/cover_'.$pic.'.jpg');
+        return view('login.login_page', ['pic' => $pic, 'pathcover' => $pathPic]);
     }
 }
--- resources/views/login/login_page.blade.php
+++ resources/views/login/login_page.blade.php
@@ -1,110 +1,114 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        
-        <!-- Title -->
-        <title>Nadha School (Aplikasi Manajemen Sekolah) | Login Page</title>
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-        <meta charset="UTF-8">
-        <meta name="description" content="Responsive Admin Dashboard Template" />
-        <meta name="keywords" content="admin,dashboard" />
-        <meta name="author" content="Steelcoders" />
-        
-        <!-- Styles -->
-        <link type="text/css" rel="stylesheet" href="{{ asset('') }}ladun/annisa/plugins/materialize/css/materialize.min.css"/>
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="{{ asset('') }}ladun/annisa/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">        
+<html>
 
-        	
-        <!-- Theme Styles -->
-        <link href="{{ asset('') }}ladun/annisa/css/alpha.min.css" rel="stylesheet" type="text/css"/>
-        <link href="{{ asset('') }}ladun/annisa/css/custom.css" rel="stylesheet" type="text/css"/>
-        
-        
-    </head>
-    <body class="signin-page">
-        <div class="loader-bg"></div>
-        <div class="loader">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
-                    <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>NadhaSchool - Homepage</title>
+
+    <meta name="description" content="Nadha School - Aplikasi Manajemen Sekolah">
+    <meta name="author" content="haxorsprogrammingclub">
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Stylesheets -->
+
+    <!-- Fonts and Codebase framework -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
+    <link rel="stylesheet" id="css-main" href="{{ asset('ladun/login/assets/css/codebase.min.css') }}">
+    <link rel="stylesheet" id="css-main" href="{{ asset('ladun/login/assets/css/login.css') }}">
+
+</head>
+
+<body>
+
+    <div id="page-container" class="main-content-boxed">
+
+        <!-- Main Container -->
+        <main id="main-container">
+            <!-- Page Content -->
+            <div class="bg-image" style="background-image: url('{{$pathcover}}')">
+                <div class="row mx-0 bg-black-op">
+                    <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
+                        <div class="p-30 invisible" data-toggle="appear">
+                            <p class="font-size-h3 font-w600 text-white">
+                                Spirit of Collaborative, never stop coding.
+                            </p>
+                            <p class="font-italic text-white-op">
+                                Copyright &copy; Haxorsprogrammingclub - Support by NadhaMedia
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="spinner-layer spinner-red">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
-                    <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                    </div>
-                </div>
-                <div class="spinner-layer spinner-yellow">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
-                    <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                    </div>
-                </div>
-                <div class="spinner-layer spinner-green">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
-                    <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
+                    <div class="hero-static col-md-6 col-xl-4 d-flex align-items-center bg-white invisible" data-toggle="appear" data-class="animated fadeInRight">
+                        <div class="content content-full">
+                            <!-- Header -->
+                            <div class="px-30 py-10">
+                                <div class="teksTengah">
+                                    <img src="{{ asset('ladun/annisa/images/nadha_school.jpg') }}" class="wd200">
+                                </div>
+                                <!-- <h1 class="h3 font-w700 mt-30 mb-10">Welcome to NadhaSchool</h1>
+                                <h2 class="h5 font-w400 text-muted mb-0">Please sign in</h2> -->
+                            </div>
+
+                            <div class="js-validation-signin px-30" id="divForm">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <div class="form-material floating">
+                                            <input type="text" class="form-control" id="txtUsername">
+                                            <label for="login-username">Username</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <div class="form-material floating">
+                                            <input type="password" class="form-control" id="txtPassword">
+                                            <label for="login-password">Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <a class="btn btn-sm btn-hero btn-alt-primary" @click="loginAtc">
+                                        <i class="si si-login mr-10"></i> Sign In
+                                    </a>
+                                    <div class="mt-30">
+                                        <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="#!">
+                                            <i class="fa fa-plus mr-5"></i> Create Account
+                                        </a>
+                                        <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="#!">
+                                            <i class="fa fa-warning mr-5"></i> Forgot Password
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END Sign In Form -->
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="mn-content valign-wrapper">
-            <main class="mn-inner container">
-                <div class="valign">
-                      <div class="row">
-                          <div class="col s12 m6 l4 offset-l4 offset-m3">
-                              <div class="card white darken-1">
-                                  <div class="card-content">
-                                      <h4>Nadha School</h4>
-                                      <h5>Aplikasi Manajemen Sekolah</h5>
-                                      <span class="card-title">Sign In</span>
-                                       <div class="row">
-                                           <form class="col s12">
-                                               <div class="input-field col s12">
-                                                   <input id="email" type="email" class="validate">
-                                                   <label for="email">Email</label>
-                                               </div>
-                                               <div class="input-field col s12">
-                                                   <input id="password" type="password" class="validate">
-                                                   <label for="password">Password</label>
-                                               </div>
-                                               <div class="col s12 right-align m-t-sm">
-                                                   <a href="sign-up.html" class="waves-effect waves-grey btn-flat">sign up</a>
-                                                   <a href="index.html" class="waves-effect waves-light btn teal">sign in</a>
-                                               </div>
-                                           </form>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                    </div>
-                </div>
-            </main>
-        </div>
-        
-        <!-- Javascripts -->
-        <script src="{{ asset('') }}ladun/annisa/plugins/jquery/jquery-2.2.0.min.js"></script>
-        <script src="{{ asset('') }}ladun/annisa/plugins/materialize/js/materialize.min.js"></script>
-        <script src="{{ asset('') }}ladun/annisa/plugins/material-preloader/js/materialPreloader.min.js"></script>
-        <script src="{{ asset('') }}ladun/annisa/plugins/jquery-blockui/jquery.blockui.js"></script>
-        <script src="{{ asset('') }}ladun/annisa/js/alpha.min.js"></script>
-        
-    </body>
+            <!-- END Page Content -->
+        </main>
+        <!-- END Main Container -->
+    </div>
+    <!-- END Page Container -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/vue@next"></script>
+
+    <script src="{{ asset('ladun/login/assets/js/codebase.core.min.js') }}"></script>
+    <script src="{{ asset('ladun/login/assets/js/codebase.app.min.js') }}"></script>
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('ladun/login/assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <!-- Page JS Code -->
+    <script src="{{ asset('ladun/login/assets/js/pages/op_auth_signin.min.js') }}"></script>
+
+    <!-- Custom JS Code  -->
+    <script>
+        const server = "{{ url('') }}/";
+    </script>
+
+    <script src="{{ asset('ladun/login/assets/js/login.js') }}"></script>
+
+</body>
+
 </html>
